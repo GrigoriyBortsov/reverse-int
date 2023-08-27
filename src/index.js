bfa80791182ee465a12eebc9969516ff4be2c7c4
GrigoriyBortsov <130370766+GrigoriyBortsov@users.noreply.github.com>
2023-08-27 12:03:00 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 module.exports = function reverse (n) {
-  
+    const digits = String(n).split('');
+    const reversedDigits = digits.reverse();
+    return parseInt(reversedDigits.join(''), 10);
 }
